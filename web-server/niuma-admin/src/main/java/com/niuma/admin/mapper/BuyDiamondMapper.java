22e670835f4ff9b8a7caf529c27261d111b8cf58
AthenDrakomin-hub <AthenDrakomin-hub@users.noreply.github.com>
2026-04-07 15:05:37 +0000

    feat: 拉取项目到工作区
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- web-server/niuma-admin/src/main/java/com/niuma/admin/mapper/BuyDiamondMapper.java
+++
@@ -1,9 +0,0 @@
-package com.niuma.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.niuma.admin.entity.BuyDiamond;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuyDiamondMapper extends BaseMapper<BuyDiamond> {
-}
--- web-server/niuma-common/src/main/java/com/niuma/common/validation/Edit.java
+++
@@ -1,9 +0,0 @@
-package com.niuma.common.validation;
-
-/**
- * @Author: mtm
- * @Date: 2023/6/28 17:00
- * @title: Edit
- */
-public interface Edit {
-}
